a59d7b0c00cd2fc2a5196cd8652690ff2f553776
Grace Wangui <wanguigrace280@gmail.com>
2022-08-19 23:11:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names=['Guadalupe', 'Ollie', 'Aki']
+const event='surprise'
+
+function writeCards(names,event){
+    let thankYou=[]
+    for(let i=0;i<names.length;i++){
+       thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+       
+    }
+    return thankYou;
+}
+
+function countDown(){
+   let countDown=11
+    while(countDown--){
+        console.log(countDown) ;
+      
+      }
+        
+
+}
